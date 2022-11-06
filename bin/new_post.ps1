744fa2b054a59c776729e5c1eebb0405c34c9fe4
funduck <qlfunduck@gmail.com>
2022-11-06 20:48:28 +0300

    date

--- bin/new_post.ps1
+++ bin/new_post.ps1
@@ -1,6 +1,6 @@
 param ($postname)
 $name = (get-date -format "yyyy-dd-MM")+"-"+$postname+".md"
-$date = (get-date -format "yyyy-dd-MM hh:mi:ss")
+$date = (get-date -format "yyyy-dd-MM HH:mm:ss zzz")
 Write-Output @"
 ---
 layout: post
